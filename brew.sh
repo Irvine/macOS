cba7a91fae2844ab645ad59147bef92c6923df78
Chris Kraynik <chris@kraynik.com>
2019-04-27 12:39:20 -0700

    Commit 1: install homebrew/apps/tools/dependencies

---
+++ brew.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Homebrew for programmatic software installation
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install command line tools and dependencies
+brew install bitwarden-cli nmap node speedtest-cli
+
+# Install graphical applications, macOS extensions, and dependencies
+brew cask install authy avibrazil-rdm bitwarden box-drive google-backup-and-sync google-chrome gpg-suite-no-mail keka keybase qlvideo skype slack transmission vlc
