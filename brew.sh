1f37e7e685ea4aace91911ff832fabf621e52387
Chris Kraynik <chris@kraynik.com>
2019-10-27 18:50:57 -0700

    Removed node; already a bitwarden-cli dependency

--- brew.sh
+++ brew.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install command line tools and dependencies
-brew install bitwarden-cli kryptco/tap/kr nmap node speedtest-cli
+brew install bitwarden-cli kryptco/tap/kr nmap speedtest-cli
 
 # Install graphical applications, macOS extensions, and dependencies
 brew cask install authy avibrazil-rdm bitwarden box-drive google-backup-and-sync google-chrome gpg-suite-no-mail keka keybase qlvideo skype slack transmission vlc
